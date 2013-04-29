1e5e34f5c428c791351e862156d21bef5eaf4db2
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-29 15:07:59 +0200

    Starting challenge 2

---
+++ 2.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+	Tuenti Programming Challenge 3
+	https://contest.tuenti.net/
+	
+	@author: shoghicp
+	@email: shoghicp@gmail.com
+	
+	This must be run using at least PHP 5.4,
+	enabling BCMath and the pthreads extension.
+	TuentiLib: http://bit.ly/14jgAPV
+	Windows binaries: http://bit.ly/14j6jTO
+*/
+
+//Challenge 2 - Did you mean...?
+//Solution: 
+
+
+define("CHALLENGE", "2");
+include("TuentiLib.php");
