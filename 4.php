a6c7a0a2b3eea0a288bbda569f40e4c1b91bc6c7
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-29 22:13:01 +0200

    Get the random data block end/start

--- 4.php
+++ 4.php
@@ -18,3 +18,49 @@
 
 define("CHALLENGE", "4");
 include("TuentiLib.php");
+$f = fopen("dictionaries/integers", "r"); //Little-Endian integers. That makes everything easier due to the patterns ;)
+//Start of high byte in order: 399804
+
+
+//Get the sequence start ;)
+$sequenceStart = 0;
+$cnt = 0;
+fseek($f, 0);
+while(true){	
+	$n = TuentiLib::readLInt(fread($f, 4));
+	if($cnt > 16){
+		TuentiLib::debug(__LINE__, "sequenceStart offset $sequenceStart");
+		break;
+	}elseif(($n >> 24) !== 0x7F){
+		if($sequenceStart === 0){
+			$sequenceStart = ftell($f) - 4;
+		}
+		++$cnt;
+	}else{
+		$sequenceStart = 0;
+		$cnt = 0;
+	}
+}
+
+//Get the sequence end
+$sequenceEnd = 0;
+$cnt = 0;
+fseek($f, -4, SEEK_END);
+while(true){	
+	$n = TuentiLib::readLInt(fread($f, 4));
+	if($cnt > 16){
+		TuentiLib::debug(__LINE__, "sequenceEnd offset $sequenceEnd");
+		break;
+	}elseif(($n >> 24) !== 0x7F){
+		if($sequenceEnd === 0){
+			$sequenceEnd = ftell($f) + 4;
+		}
+		++$cnt;
+	}else{
+		$sequenceEnd = 0;
+		$cnt = 0;
+	}
+	fseek($f, -8);
+}
+
+fclose($f);
