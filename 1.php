81f34079f62ed23c0c1c60b13d5fb479ceaf9274
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-29 15:00:09 +0200

    Removed debug

--- 1.php
+++ 1.php
@@ -22,7 +22,6 @@ include("TuentiLib.php");
 $cases = (int) trim(TuentiLib::getLine());
 
 for($case = 1; $case <= $cases; ++$case){
-	TuentiLib::debug(__LINE__, "case $case");
 	$budget = (int) trim(TuentiLib::getLine());
 	$bitCoins = 0;
 	$exchangeRates = array_map("intval", explode(" ", trim(TuentiLib::getLine())));
@@ -32,22 +31,22 @@ for($case = 1; $case <= $cases; ++$case){
 	$offset = 0;
 	for($offset = 0; $offset < $count; ++$offset){
 		$current = $exchangeRates[$offset];
+		if($current === 0){ //Troll!
+			continue;
+		}
 		if(($offset === 0 or $exchangeRates[$offset - 1] >= $current) and ($offset !== $limit and $exchangeRates[$offset + 1] >= $current)){
 			//Min, buy
 			if($current <= $budget){
 				$buy = (int) ($budget - ($budget % $current));
 				$bitCoins += (int) ($buy / $current);
 				$budget -= $buy;
-				TuentiLib::debug(__LINE__, "buy $current $buy");
 			}
 		}elseif(($offset !== 0 and $exchangeRates[$offset - 1] <= $current) and ($offset === $limit or $exchangeRates[$offset + 1] <= $current)){
 			//Max, sell
 			$sell = (int) ($bitCoins * $current);
 			$budget += $sell;
 			$bitCoins = 0;
-			TuentiLib::debug(__LINE__, "sell $current $sell");
-		}
-		TuentiLib::debug(__LINE__, "bitcoins $bitCoins");		
+		}		
 	}
 	echo $budget.PHP_EOL;
 }
