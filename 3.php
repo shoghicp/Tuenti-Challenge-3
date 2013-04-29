1bb2d46afa73f080a80f248b0ad79d530338fe6d
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-29 20:28:02 +0200

    Submitted challenge #3

--- 3.php
+++ 3.php
@@ -24,78 +24,94 @@ $cases = intval(trim(TuentiLib::getLine()));
 for($case = 1; $case <= $cases; ++$case){
 	$scenes = null;
 	preg_match_all('#[\.\<\>][^\.\<\>]*#', trim(TuentiLib::getLine()), $scenes); //Get the different scenes with the type character.
+	$safe = array();
 	$order = array();
 	$index = 0;
 	$error = false;
+	if($scenes[0]{0} === "<"){ //No start
+		$error = "invalid";
+	}
 	foreach($scenes[0] as $s){
 		$type = $s{0};
 		$text = substr($s, 1);
 		switch($type){
 			case ".":
 				if(isset($order[$text])){
-					if($order[$text][1] > $index or $order[$text][2] < $index){
+					if($order[$text][0] > $index or $order[$text][1] < $index){
 						$error = "invalid";
+						break;
 					}
+					unset($order[$text]);
+				}elseif(isset($safe[$text])){
+					$error = "invalid";
+					break;
 				}
-				$order[$text] = array($index, false, false);
+				$safe[$text] = $index;
 				break;
 			case ">":
+				if(isset($safe[$text])){
+					$error = "invalid";
+					break;
+				}
 				if(isset($order[$text])){
-					if($order[$text][1] === false and $order[$text][2] === false){
-					}elseif($order[$text][1] !== false){
-						$order[$text] = array($index, max($order[$text][1], $index), $order[$text][2]);
-					}else{
-						$order[$text] = array($index, $index, $order[$text][2]);
-					}
+					$order[$text] = array(max($index, $order[$text][0]), $order[$text][1]);
 				}else{
-					$order[$text] = array($index, $index, false);
+					$order[$text] = array($index, 2147483647);
 				}
 				break;
 			case "<":
+				if(isset($safe[$text])){ //Is valid ;)
+					break;
+				}
 				if(isset($order[$text])){
-					if($order[$text][1] === false and $order[$text][2] === false){
-					}elseif($order[$text][2] !== false){
-						$order[$text] = array($index, $order[$text][1], min($order[$text][2], $index));
-					}else{
-						$order[$text] = array($index, $order[$text][1], $index);
+					$order[$text] = array($order[$text][0], min($index - 2, $order[$text][1]));
+					if($order[$text][0] === $order[$text][1]){
+						/*$error = "invalid";
+						break;*/
 					}
 				}else{
-					$order[$text] = array($index, false, $index);
+					$order[$text] = array(-1, $index - 2);
 				}
 				break;
 		}
 		++$index;
+		if($error !== false){
+			break;
+		}
+	}
+	if($error === false and $type === ">"){ //No end
+		$error = "invalid";
+	}
+	$min = count($safe) + count($order);
+	foreach($order as $text => $data){
+		$order[$text] = range($data[0], min($min, $data[1]));
+	}
+	$changes = count($order);
+	while($changes > 0 and $error === false){
+		$changes = 0;
+		foreach($order as $text => $range){
+			foreach($range as $i => $n){
+				if(in_array($n, $safe, true)){
+					unset($order[$text][$i]);
+					++$changes;
+				}
+			}
+			if(count($order[$text]) === 1){
+				$index = array_pop($order[$text]);
+				$safe[$text] = $index;
+				unset($order[$text]);
+				++$changes;
+			}elseif(count($order[$text]) === 0){
+				$error = "valid";
+				break;
+			}
+		}
 	}
 	if($error !== false){
 		echo $error.PHP_EOL;
 		continue;
 	}
-	$newOrder = array();
-	$cnt = count($order) - 1;
-	foreach($order as $text => $data){
-		if($data[1] === false and $data[2] === false){
-			$newOrder[$text] = $data[0] + 1;
-		}elseif($data[1] !== false and $data[2] !== false and ($data[2] - $data[1]) == 2){
-			$newOrder[$text] = $data[1] + 2;
-		}elseif($data[1] !== false and $data[2] !== false and ($data[2] - $data[1]) > 2){
-			$error = "valid";
-		}elseif($data[1] !== false and $data[2] !== false and ($data[2] - $data[1]) < 2){
-			$error = "invalid";
-		}elseif($data[1] === false and $data[2] <= 1){
-			$newOrder[$text] = 0;
-		}elseif($data[1] === false){
-			$error = "valid";
-		}elseif($data[2] === false and $data[1] >= $cnt){
-			$newOrder[$text] = $cnt + 1;
-		}elseif($data[2] === false){
-			$error = "valid";
-		}
-	}
-	asort($newOrder);
-	$ordered = array_flip($newOrder);	
-	if($error === false){
-		echo implode(",",$ordered).PHP_EOL;
-	}else{
-		echo $error.PHP_EOL;
-	}
+	asort($safe);
+	echo implode(",", array_flip($safe)).PHP_EOL;
+	
 }
