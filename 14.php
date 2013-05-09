bbec839c8c517119ddf54d8c2f20c4f829c19db5
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-05-09 19:57:48 +0200

    Actualizada la solución 14

--- 14.php
+++ 14.php
@@ -18,7 +18,7 @@
 define("CHALLENGE", "14");
 include("TuentiLib.php");
 
-/*
+
 $hex = array();
 $hex[] = TuentiLib::hexToStr("5cb4290b0a901940ac5392061f0714b9109ae565951780dc81171f50fbd919aba08c1d9dce850618bd46d9237a578eae5b9f71f8b2312719a0bf05800e808c9007e24c1bef35fb8aaa07681557842453bb3a81c30d28ecb5d295bc24739b9eb6405d83d4ab05cd82b3b238720a569bb1d526f4132271188578862008c3afae028c793be44e26ff46397a9b62bde49a2df643e6ed755b8c6e48c785d008c9855f2c5d5dea2bb184fa1c7a070d2c39f596190d5fd10021c337a32eb8480e0c7bf0");
 $hex[] = TuentiLib::hexToStr("5cfa390116d21e14b25c9b1e465f5bf8129ae27e94088b9bc80e1d1fe4c21ca2bb9644dcc9990618a64edc66655d82f9479d7ba0f7586e1de1b718ce139bc9df44de4d17ea70b290e70b27114697311aac2cccd10a28f7a9db9abc3e7480cbb55140c193f544f6d6bebe23391f0480a99b34bb156d2111837e8f6547dae7a84ccf6c3da1002bec42392e9a27bfe7d722ea11a5f568508a7911de88db4ac9a41e2d5913f26ea39ffa047b18066b25b5");
@@ -32,17 +32,25 @@ $hex[] = TuentiLib::hexToStr("5cfa6d0205d4194cfb4694171f1e1eeb14d9ec6f84418a9ac8
 $hex[] = TuentiLib::hexToStr("46f724010ac30413af41dc1a5e051eb91298e16f850d84888d1e524be1cc0ce7bd90019dd9851519ab44c22378488eaa5b807fb7fa116909e1a21e800a8ed8940afe4907b931b990b218634742863c4fae2fcdce453ffba9c689f5247bcfdfa05c1480dae949edcdb9ae7621115683bede7cf425382554816f846904c4eea91f8c6532f70b6ef946276d9d2eb3f6df24af17ede3741595755dc689d15dc4b81076535dfb2ba683bb1e70141b2c35e9d900430acc596fcb29a77cb84c06077cfe32a797f77fc5f1843795ce");
 $hex[] = TuentiLib::hexToStr("41fc284417c30212a2128f1d1f151aeb4bd9c462d0158d99c8181758e0c316aea79f44c9d2885422a648c766655dcbf9438c69e3f10a620fb5b4158e5abbc49817aa4d1fea70b682a30f27060389274eef2cc797153fecb0d998bc3c799dc7f2585a8ac1fc05e5ccb3fd34301b18cca7d236b10b3471068969827203c8ebe70ddf2d32a10c2ffe0726619e27fc");
 $hex[] = TuentiLib::hexToStr("54b42e0b09da020efb5f95014b1210fc518de56d84419599870a1e5aa9c019acacd813d5df835403ba58d86d700edab614897fb0fb1f694eb2be1cc50e87c59f03aa4611f420b786b30f6b1e03832755a333d3d80a3ca3a9c6dde8253c9ad0b65c4688c0f14ce9c3a3b876211613ccb9d535b109383800952e8c6647cee0aa1cc06827e44e28f548277dc6");
-$hex[] = TuentiLib::hexToStr("58ed6d000bd4190fa9128f1346005bed1998f92cb9418d9d9e1f525ea9c019abaf9716d0df895407bd43dd6a7403caac40943aa4fe19690ae1b01fc45a8e8c9f05fe500cf83cfb87a20c6e044a802659b663c8d94537ecb2d491bc2c758dccb7195583d7a551ecc3a3fd1f751f1bcca4d337a6022b3e06892e867804d8fca2088c6b21ee036ee9463d678625f2f7d429f906f7f16546d6");*/
+$hex[] = TuentiLib::hexToStr("58ed6d000bd4190fa9128f1346005bed1998f92cb9418d9d9e1f525ea9c019abaf9716d0df895407bd43dd6a7403caac40943aa4fe19690ae1b01fc45a8e8c9f05fe500cf83cfb87a20c6e044a802659b663c8d94537ecb2d491bc2c758dccb7195583d7a551ecc3a3fd1f751f1bcca4d337a6022b3e06892e867804d8fca2088c6b21ee036ee9463d678625f2f7d429f906f7f16546d6");
 
 $ciphertext = TuentiLib::hexToStr("46fb200144df180dba5c8f52481c0ef515d9e963d0008b859c121b51ee8d0ca8e98b01d89a841257a1559174765d8ea95b9e69aaf014624eb5be51c415cfc5854aaa6c18b929b496e71a721303846856ae31c6d24529f4a9c19ef46a75819ea156598893e644f2c7f7ae39381b0184b5c937f8473a3800842e822014c4e8a94cc36373e81a6ee94632678625f2a5ff2eeb4eeae42d4190791cfd8fcc5f8dec2c2c5547fd63ebcb8a3c56303b4976dff9502d30e85955ed128114eb094b1667bb7da282be7ed7f1873d8e8c31d18ef24a48e990db46f5a591ecbc33704380a1b9b8babcdb4fe3");
 $plaintext = "Some humans would do anything to see if it was possible to do it. If you put a large switch in some cave somewhere, with a sign on it saying 'End-of-the-World Switch. PLEASE DO NOT TOUCH', the paint wouldn't even have time to dry.";
 $key = $ciphertext ^ $plaintext;
+TuentiLib::debug(__LINE__, "key: ".TuentiLib::strToHex($key));
+foreach($hex as $b){
+	TuentiLib::debug(__LINE__, $b ^ $key);
+}
 echo $key ^ TuentiLib::hexToStr(TuentiLib::getLine());
 die();
 
 
 //OLD - USED TO OBTAIN THE STRING
 
+$crib = "the ";
+
+
+$criblen = strlen($crib);
 $correct = array();
 for($i = 0x20; $i <= 0x7a; ++$i){
 	$correct[$i] = true;
@@ -65,7 +73,7 @@ for($i = 0; $i < $cnt; ++$i){
 		}
 	}
 	if($continue === true){
-		/*if(((substr($co1, $i, $criblen) ^ $res) ^ substr($co2, $i, $criblen)) === $crib){
+		if(((substr($co1, $i, $criblen) ^ $res) ^ substr($co2, $i, $criblen)) === $crib){
 			$xor = substr($co2, $i, $criblen) ^ $res;
 		}else{
 			$xor = substr($co1, $i, $criblen) ^ $res;
@@ -89,9 +97,9 @@ for($i = 0; $i < $cnt; ++$i){
 				}
 			}
 		}
-		if(($valid / $invalid) > 1.15){*/
-			echo TuentiLib::strToHex($co2).$res.PHP_EOL;
-		//}
+		if(($valid / $invalid) > 1.15){
+			echo $res.PHP_EOL;
+		}
 	}
 }
 }
